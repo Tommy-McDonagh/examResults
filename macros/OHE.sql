55f31e01ef5a704aa2078b31ee4a0d95c956a036
tommymcdonagh@kubrickgroup.com <tommymcdonagh@kubrickgroup.com>
2023-05-03 14:48:40 +0000

    corrected macro to perform as required

--- macros/OHE.sql
+++ macros/OHE.sql
@@ -1,7 +1,7 @@
 {% macro one_hot_encode(column_name, values) %}
     {% for value in values %}
         CASE
-            WHEN {{ column_name }} = "{{ value }}" THEN 1
+            WHEN {{ column_name }} = '{{ value }}' THEN 1
             ELSE 0
         END as {{ column_name }}_{{ value }}
         {% if not loop.last %},{% endif %}
